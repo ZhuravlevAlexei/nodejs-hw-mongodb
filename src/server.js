78f1a797700aecd17720a6b2bd1485509f1e8849
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-05-17 19:28:49 +0300

    step 2

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+import { env } from './utils/env.js';
+const PORT = Number(env('PORT', '3000'));
+export const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res) => {
+    //без next, бо зараз помилку далі не передаємо в наступний обробник
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
