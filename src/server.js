3d7df50fa01c7241646d7f5b3866389a0514fdba
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-05-18 10:22:34 +0300

    steps 4, 5, 6

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 
+import { getAllContacts, getContactById } from './services/contacts.js';
 import { env } from './utils/env.js';
 const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
@@ -17,9 +18,26 @@ export const setupServer = () => {
     }),
   );
 
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
   app.use('*', (req, res) => {
-    //без next, бо зараз помилку далі не передаємо в наступний
-    //обробник, а eslint на next без виклику свариться
     res.status(404).json({
       message: 'Not found',
     });
