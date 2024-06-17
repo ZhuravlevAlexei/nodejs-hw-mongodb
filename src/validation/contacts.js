a45c820bdd67c341317444c3b2e6c7ea3f6cd777
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-06-17 15:12:47 +0300

    email length

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(3).max(20).required(),
+  email: Joi.string().min(5).max(30).required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 });
